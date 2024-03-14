be929007a956aac6298f7cfe07c8d69afdb0b8d3
Andrei Cosmin Radu <andreicos1@yahoo.com>
2024-03-14 20:16:47 +0200

    add code for uploading data to db

--- utils/upload-data.ts
+++ utils/upload-data.ts
@@ -1,3 +1,17 @@
-export const uploadTeams = async (data: any) => {}
+import { SupabaseClient } from "@supabase/supabase-js"
 
-export const uploadPlayers = async (data: any) => {}
+interface TeamData {
+  name: string
+}
+export const uploadTeams = async (supabase: SupabaseClient, data: TeamData[]) => {
+  await supabase.from("team").insert(data)
+}
+
+interface PlayerData {
+  name: string
+  jersey: string
+  team_id: string
+}
+export const uploadPlayers = async (supabase: SupabaseClient, data: PlayerData[]) => {
+  await supabase.from("player").insert(data)
+}
