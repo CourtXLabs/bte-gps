ac3fb984c0a89620fc70fa05e7944d082669350f
Andrei Cosmin Radu <andreicos1@yahoo.com>
2024-09-24 13:11:29 +0300

    update header style

--- components/sections/header/Header.tsx
+++ components/sections/header/Header.tsx
@@ -8,23 +8,27 @@ import ProfileInfo from "./ProfileInfo"
 export default async function Header() {
   const isAdmin = await getIsAdmin()
   return (
-    <div className="w-full bg-black p-4 lg:px-0">
+    <div className="w-full bg-black p-4">
       <div className="mx-auto flex max-w-screen-2xl items-center gap-6 lg:gap-12">
-        <Link href="/">
-          <Image src="/logo.jpeg" alt="logo" width={200} height={60} />
-        </Link>
-        <Link href="/about-us" className={cn(buttonVariants({ variant: "link" }), "text-xl text-foreground")}>
+        <div className="relative">
+          <Link href="/">
+            <Image src="/logo.jpeg" alt="logo" width={200} height={60} />
+          </Link>
+          <span className="absolute -right-12 bottom-0 rounded-2xl bg-primary px-2 py-1 text-sm leading-none text-black">
+            Beta
+          </span>
+        </div>
+        <Link href="/about-us" className={cn(buttonVariants({ variant: "link" }), "ml-auto text-xl text-foreground")}>
           About Us
         </Link>
         <Link href="/glossary" className={cn(buttonVariants({ variant: "link" }), "text-xl text-foreground")}>
           Glossary
         </Link>
         {isAdmin && (
-          <Link href="/add-game" className={cn(buttonVariants({ variant: "link" }), "text-xl text-foreground")}>
+          <Link href="/add-game" className={cn(buttonVariants({ variant: "link" }), "mr-auto text-xl text-foreground")}>
             Add Game
           </Link>
         )}
-        <p className="ml-auto text-lg font-medium text-gray-300">BETA</p>
         <ProfileInfo />
       </div>
     </div>
--- components/sections/header/ProfileInfo.tsx
+++ components/sections/header/ProfileInfo.tsx
@@ -1,8 +1,14 @@
+import { UserIcon } from "lucide-react"
+
 export default function ProfileInfo() {
   return (
-    <div>
-      <div>
-        <p className="text-sm font-bold leading-none"></p>
+    <div className="flex items-center gap-2.5">
+      <div className="flex h-11 w-11 items-center justify-center rounded-full bg-muted">
+        <UserIcon size={24} className="fill-white" />
+      </div>
+      <div className="space-y-1.5 text-sm">
+        <p className="font-bold leading-none">Asfandyar Butt</p>
+        <p className="leading-none">asfandyar452gmail.com</p>
       </div>
     </div>
   )
