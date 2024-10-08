5aadf27cefff832a73f6aa25dcf5d6a41e0dd7ae
Andrei Cosmin Radu <andreicos1@yahoo.com>
2024-10-08 13:05:10 +0300

    add temp redirect from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function LandingPage() {
-  return <div>LandingPage</div>
+import { getIsLoggedIn } from "@/lib/auth"
+import { redirect } from "next/navigation"
+
+export default async function LandingPage() {
+  const isLoggedIn = await getIsLoggedIn()
+  if (isLoggedIn) {
+    return redirect("/players/41")
+  } else {
+    redirect("/login")
+  }
 }
