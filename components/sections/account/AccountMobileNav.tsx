d0c8c660e5c274f2cc054ac62836982699ba0794
Andrei Cosmin Radu <andreicos1@yahoo.com>
2024-10-28 12:18:02 +0200

    wip mobile account pages

--- components/sections/account/AccountMobileNav.tsx
+++ components/sections/account/AccountMobileNav.tsx
@@ -3,13 +3,13 @@ import { Card, CardContent } from "@/components/ui/card"
 import { getUserData } from "@/lib/auth"
 import AccountLinks from "./AccountLinks"
 
-export default async function AccountSidebar() {
+export default async function AccountMobileNav() {
   const userData = await getUserData()
   const userFullName = userData?.user_metadata?.name
   const firstName = userFullName?.split(" ")[0]
   return (
-    <Card className="lg:h-[45rem]">
-      <CardContent className="flex h-full w-full flex-col gap-8 pt-6 text-lg lg:w-72">
+    <Card className="hidden h-[45rem] lg:block">
+      <CardContent className="flex h-full w-72 flex-col gap-8 pt-6 text-lg">
         <div>
           <span className="font-light">Welcome</span>{" "}
           <span className="block break-words pb-1 text-3xl font-bold">{firstName || "Customer"}</span>
