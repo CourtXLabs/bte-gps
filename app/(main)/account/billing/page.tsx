f618de06556c96a9f8fb229c0612de19f729af14
Andrei Cosmin Radu <andreicos1@yahoo.com>
2024-11-23 17:49:43 +0200

    add stripe customer portal link

--- app/(main)/account/billing/page.tsx
+++ app/(main)/account/billing/page.tsx
@@ -1,19 +1,18 @@
 import { buttonVariants } from "@/components/ui/button"
+import { STRIPE_CUSTOMER_PORTAL_LINK } from "@/constants/pricing"
 import { getUserEmail } from "@/lib/auth"
 import { cn } from "@/lib/utils"
 import Link from "next/link"
 
-const billingInfoLink = "https://billing.stripe.com/p/login/test_cN2bKffUqalD03S144"
-
-export default function AccountBillingPage() {
-  const userEmail = getUserEmail()
+export default async function AccountBillingPage() {
+  const userEmail = await getUserEmail()
 
   return (
     <div className="space-y-6">
       <h1 className="text-xl font-bold">Billing Information</h1>
       <Link
         target="_blank"
-        href={`${billingInfoLink}?prefilled_email=${userEmail}`}
+        href={`${STRIPE_CUSTOMER_PORTAL_LINK}?prefilled_email=${userEmail}`}
         className={cn(buttonVariants({ size: "xl" }), "text-base")}
       >
         Change your plan
