7407ac56e9ff22f466b16952de5c0be1f281b9d8
Andrei Cosmin Radu <andreicos1@yahoo.com>
2024-12-16 13:47:33 +0200

    update contact us copy

--- app/(main)/contact-us/page.tsx
+++ app/(main)/contact-us/page.tsx
@@ -9,7 +9,9 @@ export default async function page() {
     <main className="mx-auto mb-20 mt-20 max-w-screen-2xl px-6 lg:mb-24 lg:mt-28">
       <div>
         <h1 className="text-5xl lg:text-6xl">Need help? Get in touch</h1>
-        <p className="mt-4 text-lg text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className="mt-4 text-lg text-gray-400">
+          Have some questions for team BTE Analytics? Send us the message by filling the form below.
+        </p>
       </div>
       <div className="mt-10 flex flex-col gap-12 lg:mt-20 lg:flex-row lg:items-start 2xl:gap-24">
         <ContactUsForm email={email} name={name} />
